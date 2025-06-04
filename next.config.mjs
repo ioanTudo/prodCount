30e8de331606cfb07eaa38c43ad593d490575e23
Ioan Tudor <ioantudor@Laptop-Ioan.local>
2025-06-04 23:45:56 +0300

    new

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  basePath: "/productCount",
+  basePath: isProd ? "/prodCount" : "",
   images: {
     unoptimized: true,
   },
